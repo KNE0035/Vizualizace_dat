9deef7e66932215168fc0993be84db53bbfbc684
Marek Kneys <kne0035@vsb.cz>
2019-12-15 12:12:07 +0100

    change

--- cv7.py
+++ cv7.py
@@ -4,10 +4,10 @@ import cv2
 from os import listdir
 import random
 
-spaceSize = (250, 250)
+spaceSize = (500, 500)
 
 N = 4
-E = 4
+E = 6
 k = math.sqrt((spaceSize[0] * spaceSize[1]) / N)
 
 class Vector2d:
@@ -22,7 +22,7 @@ class Vector2d:
 
     def dist(self, other):
         minusPoint = self.substract(other)
-        return minusPoint.nor
+        return minusPoint.norm()
 
     def norm(self):
         return math.sqrt(math.pow(self.x, 2) + math.pow(self.y, 2))
@@ -50,13 +50,16 @@ def main():
     points = pointsEdges[0]
     edges = pointsEdges[1]
 
-    t = 2
-    iter = 30
-    dt = 0.2
+    #edges = [(0, 1), (1, 2), (2, 3)]
+
+    t = 0.1
+    iter = 5000
+    dt = 0.00001
     drawGraph(points, edges, 0)
     for it in range(iter):
-        fi = 0
+        #print(it)
         for i in range(N):
+            points[i].force = Vector2d(0, 0)
             for j in range(N):
                 if i != j:
                     points[i].force = points[i].force.add(Fr(points[i].point, points[j].point))
@@ -69,22 +72,20 @@ def main():
 
         for forcePoint in points:
             forcePoint.point = forcePoint.point.add(forcePoint.force.scalarMultiply(1 / forcePoint.force.norm()).scalarMultiply(min(t, forcePoint.force.norm())))
-            forcePoint.point.x = int(forcePoint.point.x)
-            forcePoint.point.y = int(forcePoint.point.y)
-            drawGraph(points, edges, 10)
+            #drawGraph(points, edges, 1)
         t = t - dt
     drawGraph(points, edges, 0)
 
 def Fr(p1, p2):
-    return p2.substract(p1).scalarMultiply(- (k * k) / math.pow(p1.substract(p2).norm(), 2))
+    return p2.substract(p1).scalarMultiply(- (k * k) / math.pow(p1.dist(p2), 2))
 
 def Fa(p1, p2):
-    return p2.substract(p1).scalarMultiply(p1.substract(p2).norm() / k)
+    return p1.substract(p2).scalarMultiply(p1.dist(p2) / k)
 
 def generateGraph(nPoints):
     points = []
     edges = []
-    offset = 10
+    offset = spaceSize[0] / 4
 
     for i in range(nPoints):
 
@@ -97,13 +98,16 @@ def generateGraph(nPoints):
                 points.append(point)
                 break
 
-    for i in range(nPoints):
+    for i in range(E):
         randomPointIndex = 0
 
         while True:
-            randomPointIndex = random.randint(0, nPoints - 1)
-            if i != randomPointIndex:
-                edges.append((i, randomPointIndex))
+            randomPointIndex1 = random.randint(0, nPoints - 1)
+            randomPointIndex2 = random.randint(0, nPoints - 1)
+            randomEdge = (randomPointIndex1, randomPointIndex2)
+            randomEdgeInverted = (randomPointIndex2, randomPointIndex1)
+            if randomPointIndex1 != randomPointIndex2 and randomEdge not in edges and randomEdgeInverted not in edges:
+                edges.append(randomEdge)
                 break
 
     return (points, edges)
@@ -112,10 +116,10 @@ def drawGraph(points, edges, waitms):
     graphImg = np.zeros(spaceSize, dtype="uint8")
 
     for forcePoint in points:
-        cv2.circle(graphImg, (forcePoint.point.x, forcePoint.point.y), 1, 255, -1)
+        cv2.circle(graphImg, (int(forcePoint.point.x), int(forcePoint.point.y)), 1, 255, -1)
 
     for edge in edges:
-        cv2.line(graphImg, (points[edge[0]].point.x, points[edge[0]].point.y), (points[edge[1]].point.x, points[edge[1]].point.y), 100)
+        cv2.line(graphImg, (int(points[edge[0]].point.x), int(points[edge[0]].point.y)), (int(points[edge[1]].point.x), int(points[edge[1]].point.y)), 100)
 
     cv2.imshow("Not procesed graph", graphImg)
     cv2.waitKey(waitms)
